afdccc080db61c7d6a02e79256cdf301aa44cf6b
Alexandre Araujo Pignatari <alexandre.a.pignatari@outlook.com>
2022-04-18 10:41:03 -0300

    Versão Funcional

--- exercicios2/exercicio10/app.java
+++ exercicios2/exercicio10/app.java
@@ -1,4 +1,32 @@
 package exercicio10;
 
+import java.util.Scanner;
+
 public class app {
+    public static void main(String[] args) {
+        float valorDoProduto;
+        float cotacaoDolar;
+        float valorDolar;
+        float armazenaValorFinal = 10000000;
+
+        Scanner entry = new Scanner(System.in);
+        System.out.print("Informe a cotação do dolar: ");
+        cotacaoDolar = entry.nextInt();
+
+        for (int i = 1; i <= 2; i++) {
+
+            System.out.print("Informe o valor do produto em real: ");
+            valorDoProduto = entry.nextInt();
+
+
+            valorDolar = valorDoProduto / cotacaoDolar;
+
+            if(valorDolar < armazenaValorFinal ){
+                armazenaValorFinal = valorDolar;
+                System.out.println("recebeu");
+            }
+        }
+
+        System.out.println("O valor mais baixo pago em dolar é:  " + armazenaValorFinal);
+    }
 }
