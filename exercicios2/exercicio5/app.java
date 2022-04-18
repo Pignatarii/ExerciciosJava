a4dfdb9b545c38d3ec0d559c0703ecebb310960e
Alexandre Araujo Pignatari <alexandre.a.pignatari@outlook.com>
2022-04-18 09:39:23 -0300

    Versão funcional

--- exercicios2/exercicio5/app.java
+++ exercicios2/exercicio5/app.java
@@ -7,43 +7,28 @@ public class app {
 
     public static void main(String[] args) {
 
-        var menorNome = "";
-        var maiorNome = "";
-        var armazenaMaiorNome = "";
-        var armazenaMenorNome = "";
+        var armazenaMaiorNome = "t";
+        var armazenaMenorNome = "teste";
         var nome = "";
         int a1, a2, a3;
 
-        Scanner entry1 = new Scanner(System.in);
-        System.out.print("Escreva seu nome: ");
-        nome = entry1.next();
-
-        a1 = nome.length();
-        a2 = armazenaMaiorNome.length();
-        a3 = armazenaMenorNome.length();
-
-        for (int i = 1; i <= 2; i++) {
+        for (int i = 1; i <= 3; i++) {
             Scanner entry = new Scanner(System.in);
             System.out.print("Escreva seu nome: ");
             nome = entry.next();
 
-            armazenaMaiorNome = nome;
-            armazenaMenorNome = nome;
+            a1 = nome.length();
+            a2 = armazenaMaiorNome.length();
+            a3 = armazenaMenorNome.length();
 
-//            a1 = nome.length();
-//            a2 = armazenaMaiorNome.length();
-//            a3 = armazenaMenorNome.length();
-
-            System.out.println(a3);
-
-            if (a1 < a3) {
+            if (a3 > a1) {
                 armazenaMenorNome = nome;
 
-            }else if (a1 > a2) {
+            }else if (a2 < a1) {
                 armazenaMaiorNome = nome;
             }
         }
-        System.out.println("O menor nome informado é: " + armazenaMenorNome +
-                " e o maior nome é: " + armazenaMaiorNome);
+        System.out.println("O menor nome informado é " + armazenaMenorNome +
+                " e o maior nome é " + armazenaMaiorNome);
     }
 }
